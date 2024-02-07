19070a6425ec589b7d541dda22fc1199a8ab72b4
Adam Robson <adamray312@gmail.com>
2024-02-07 07:37:40 -0800

    add database types and createClient from supabase

---
+++ src/services/client.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js"
+import { Database } from '../lib/database.types.ts'
+
+const url = import.meta.env.VITE_SUPABASE_URL as string
+const key = import.meta.env.VITE_SUPABASE_KEY as string
+
+export const supabase = createClient<Database>(url, key)
