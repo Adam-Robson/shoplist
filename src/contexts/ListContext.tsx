202b4ec89c490ced5fa316afaf0e81f33bb50253
Adam Robson <adamray312@gmail.com>
2024-02-07 08:42:04 -0800

    implement logger for reducer

--- src/contexts/ListContext.tsx
+++ src/contexts/ListContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useReducer } from 'react';
 import { reducer, initialState } from '../utilities/reducer';
 import { ContextProp, ListContextProps } from '../lib/types';
+import { log } from '../utilities/log';
 
 export const ListContext = createContext<ListContextProps>({
   state: initialState,
@@ -8,7 +9,7 @@ export const ListContext = createContext<ListContextProps>({
 });
 
 export function ListProvider({ children }: ContextProp) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(log(reducer), initialState);
 
   return (
     <ListContext.Provider value={{ state, dispatch }}>
