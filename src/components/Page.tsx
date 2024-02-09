a8ee879213ac87cc4e60ed59fa5c212495219ef7
Adam Robson <adamray312@gmail.com>
2024-02-08 17:40:01 -0800

    add new gif and change color of background

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,8 +1,11 @@
+import dot from '../assets/dot.gif'
+
 export default function Page() {
   return (
     <section className="page-section" data-testid="page-section">
       <h1>this is shoplist.</h1>
       <h2>shop till you drop.</h2>
+      <img src={dot} alt="dot gif" />
     </section>
   )
 }
